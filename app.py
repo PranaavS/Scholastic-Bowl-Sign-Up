9a81623b00a1f17d051996f3c7e1d16360ba3f78
PranaavS <pranaav.sureshkumar26@d155.org>
2024-07-07 03:32:55 -0500

    Added basic functionality

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
-from flask import Flask, render_template
-import sqlite3
+from flask import Flask, render_template, request 
+import sqlite3 
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
-# Function to connect to the database
-def get_db():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, team TEXT)''')
-    conn.commit()
-    conn.close()
 
-# Define a route and render a template
-@app.route('/')
-def index():
-    get_db()
-    print(c.fetchall())
-    return render_template('index.html')
+@app.route('/') 
+@app.route('/home') 
+def index(): 
+	connect = sqlite3.connect('database.db') 
+	cursor = connect.cursor() 
+	cursor.execute('SELECT * FROM PARTICIPANTS') 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+	data = cursor.fetchall() 
+	return render_template("home.html", data=data)
+
+
+connect = sqlite3.connect('database.db') 
+connect.execute('CREATE TABLE IF NOT EXISTS PARTICIPANTS (first_name TEXT, last_name TEXT, email TEXT, phone TEXT)') 
+
+
+@app.route('/join', methods=['GET', 'POST']) 
+def join(): 
+	if request.method == 'POST': 
+		first_name = request.form['first_name']
+		last_name = request.form['last_name'] 
+		email = request.form['email'] 
+		phone = request.form['phone'] 
+
+		with sqlite3.connect("database.db") as users: 
+			cursor = users.cursor() 
+			cursor.execute("INSERT INTO PARTICIPANTS (first_name,last_name,email,phone) VALUES (?,?,?,?)", (first_name, last_name, email, phone)) 
+			users.commit() 
+		return render_template("home.html") 
+	else: 
+		return render_template('join.html') 
+
+
+if __name__ == '__main__': 
+	app.run(debug=False) 
