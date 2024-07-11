60f5f336d9ad2e68d4998a147e38850d15f7ae52
PranaavS <pranaav.sureshkumar26@d155.org>
2024-07-11 04:43:40 -0500

    Added removing and authentication

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def index():
 
 
 connect = sqlite3.connect('database.db') 
-connect.execute('CREATE TABLE IF NOT EXISTS PARTICIPANTS (first_name TEXT, last_name TEXT, team INTEGER, email TEXT, phone TEXT)') 
+connect.execute('CREATE TABLE IF NOT EXISTS PARTICIPANTS (first_name TEXT, last_name TEXT, team INTEGER, email TEXT, phone TEXT, hashed_pass INTEGER)') 
 
 
 @app.route('/join', methods=['GET', 'POST'])
@@ -41,6 +41,7 @@ def join():
 		email = request.form['email'] 
 		phone = request.form['phone']
 		team = request.form['team']
+		hashed_pass = hash(request.form['pass'])
 
 		with sqlite3.connect("database.db") as users: 
 			cursor = users.cursor() 
@@ -63,12 +64,12 @@ def join():
 			print(capacities_dict[f"Team {team}"] < 5)
 			
 			if capacities_dict[f"Team {team}"] < 5:
-				cursor.execute("INSERT INTO PARTICIPANTS (first_name,last_name,team,email,phone) VALUES (?,?,?,?,?)", (first_name, last_name, team, email,phone)) 
+				cursor.execute("INSERT INTO PARTICIPANTS (first_name,last_name,team,email,phone,hashed_pass) VALUES (?,?,?,?,?,?)", (first_name, last_name, team, email,phone,hashed_pass)) 
 				users.commit()
 				return render_template("confirmation.html")
 			else:
 				return redirect("/full")
-		return redirect("/") 
+
 	else: 
 		team = request.args.get("team")
 		print(team)
@@ -78,5 +79,37 @@ def join():
 def full():
 	return render_template("full.html")
 
-if __name__ == '__main__': 
+@app.route('/remove', methods=['GET', 'POST'])
+def remove(): 
+	if request.method == 'POST': 
+		entry = request.form['entry'].split()
+		hashed_pass = hash(request.form['pass'])
+
+		with sqlite3.connect("database.db") as users: 
+			cursor = users.cursor() 
+			cursor.execute('SELECT * FROM PARTICIPANTS') 
+			entries = cursor.fetchall()
+			formatted_entries = [[str(person[2]), person[0], person[1]] for person in entries]
+			index = formatted_entries.index(entry)
+			print(index)
+			print("Correct pass: " + str(entries[index][5]))
+			print("Attempted pass: " + str(hashed_pass))
+			if entries[index][5] == hashed_pass:
+				cursor.execute(f"DELETE FROM PARTICIPANTS WHERE first_name='{entry[1]}' AND last_name = '{entry[2]}' AND hashed_pass = '{hashed_pass}';")
+				return render_template("leaving.html")
+			else:
+				return render_template("incorrect_password.html")
+	else: 
+		entry = request.args.get("entry").split()
+		print(entry)
+
+		with sqlite3.connect("database.db") as users: 
+			cursor = users.cursor() 
+			cursor.execute('SELECT * FROM PARTICIPANTS') 
+			entries = cursor.fetchall()
+			formatted_entries = sorted([[str(person[2]), person[0], person[1]] for person in entries])
+			print(formatted_entries)
+			return render_template('remove.html', user_entry=entry, entries=formatted_entries) 
+
+if __name__ == '__main__':
 	app.run(debug=False) 
